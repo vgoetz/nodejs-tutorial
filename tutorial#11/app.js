fb5f81f52aa077308dc73a34837b7f6b3db60430
Viktor Götz <viktorgoetz@t-online.de>
2018-03-13 22:28:11 +0100

    disable eslint errors for console usage

--- tutorial#11/app.js
+++ tutorial#11/app.js
@@ -1,9 +1,14 @@
 'use strict';
 
+/* eslint-disable no-console */
 console.log('Hallo Welt');
+/* eslint-enable no-console */
 
 const x = 23;
 
 if (x === 42) {
+  /* eslint-disable no-console */
   console.log('Die Antwort auf alles ;-)');
+  /* eslint-enable no-console */
 }
+
