82cf5ce3e74c235dd9b2d88639f2afdefaf34cbb
Viktor Götz <viktorgoetz@t-online.de>
2018-04-09 20:36:40 +0200

    add new dev dependency 'assertthat' for testing

--- tutorial#12/test/calculatorTests.js
+++ tutorial#12/test/calculatorTests.js
@@ -1,4 +1,11 @@
 'use strict';
 
-test('add returns the sum of the given numbers.');
+const assert = require('assertthat');
 
+const calculator = require('../lib/calculator');
+
+test('add returns the sum of the given numbers.', () => {
+  const sum = calculator.add(23, 42);
+
+  assert.that(sum).is.equalTo(65);
+});
