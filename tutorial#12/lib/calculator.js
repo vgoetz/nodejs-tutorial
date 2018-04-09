dea337b18165341c5996703ee3834d1303cf3733
Viktor Götz <viktorgoetz@t-online.de>
2018-04-09 21:23:10 +0200

    add async test and force using 'done' in all tests

--- tutorial#12/lib/calculator.js
+++ tutorial#12/lib/calculator.js
@@ -10,4 +10,19 @@ calculator.add = function (...numbers) {
   return numbers.reduce((sum, number) => sum + number);
 };
 
+calculator.addAsync = function (callback, ...numbers) {
+  if (!callback) {
+    throw new Error('Callback is missing.');
+  }
+  if (!numbers || numbers.length === 0) {
+    throw new Error('Numbers are missing.');
+  }
+
+  const sum = numbers.reduce((intermidiateSum, number) => intermidiateSum + number);
+
+  setTimeout(() => {
+    callback(null, sum);
+  }, 1 * 1000);
+};
+
 module.exports = calculator;
--- tutorial#12/test/calculatorTests.js
+++ tutorial#12/test/calculatorTests.js
@@ -4,20 +4,31 @@ const assert = require('assertthat');
 
 const calculator = require('../lib/calculator');
 
-test('add returns the sum of the given numbers.', () => {
+test('add returns the sum of the given numbers.', done => {
   const sum = calculator.add(23, 42);
 
   assert.that(sum).is.equalTo(65);
+  done();
 });
 
-test('add returns the sum of the given numbers even when the first number is zero.', () => {
+test('add returns the sum of the given numbers even when the first number is zero.', done => {
   const sum = calculator.add(0, 23);
 
   assert.that(sum).is.equalTo(23);
+  done();
 });
 
-test('add throws an error if numbers are missing.', () => {
+test('add throws an error if numbers are missing.', done => {
   assert.that(() => {
     calculator.add();
   }).is.throwing('Numbers are missing.');
+  done();
+});
+
+test('addAsync returns the sum of the given numbers', done => {
+  calculator.addAsync((err, sum) => {
+    assert.that(err).is.null();
+    assert.that(sum).is.equalTo(65);
+    done();
+  }, 23, 42);
 });
