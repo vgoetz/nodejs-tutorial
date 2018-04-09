0ee7f4f1b96330bf1a08a261c2f18b703c5f5f24
Viktor Götz <viktorgoetz@t-online.de>
2018-04-09 22:02:15 +0200

    add setup and teardown also for suite definition

--- tutorial#12/test/calculatorTests.js
+++ tutorial#12/test/calculatorTests.js
@@ -4,31 +4,40 @@ const assert = require('assertthat');
 
 const calculator = require('../lib/calculator');
 
-test('add returns the sum of the given numbers.', done => {
-  const sum = calculator.add(23, 42);
+suite('calculator', () => {
 
-  assert.that(sum).is.equalTo(65);
-  done();
+  suite('add', () => {
+    test('returns the sum of the given numbers.', done => {
+      const sum = calculator.add(23, 42);
+
+      assert.that(sum).is.equalTo(65);
+      done();
+    });
+
+    test('returns the sum of the given numbers even when the first number is zero.', done => {
+      const sum = calculator.add(0, 23);
+
+      assert.that(sum).is.equalTo(23);
+      done();
+    });
+
+    test('throws an error if numbers are missing.', done => {
+      assert.that(() => {
+        calculator.add();
+      }).is.throwing('Numbers are missing.');
+      done();
+    });
+  });
+  suite('add', () => {
+    test('addAsync returns the sum of the given numbers', function (done) {
+      this.timeout(5 * 1000);
+
+      calculator.addAsync((err, sum) => {
+        assert.that(err).is.null();
+        assert.that(sum).is.equalTo(65);
+        done();
+      }, 23, 42);
+    });
+  });
 });
 
-test('add returns the sum of the given numbers even when the first number is zero.', done => {
-  const sum = calculator.add(0, 23);
-
-  assert.that(sum).is.equalTo(23);
-  done();
-});
-
-test('add throws an error if numbers are missing.', done => {
-  assert.that(() => {
-    calculator.add();
-  }).is.throwing('Numbers are missing.');
-  done();
-});
-
-test('addAsync returns the sum of the given numbers', done => {
-  calculator.addAsync((err, sum) => {
-    assert.that(err).is.null();
-    assert.that(sum).is.equalTo(65);
-    done();
-  }, 23, 42);
-});
