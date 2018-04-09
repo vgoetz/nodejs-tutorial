029be64f9f54ea740ff6aff7f1da8cd06fd107e0
Viktor Götz <viktorgoetz@t-online.de>
2018-04-09 20:58:29 +0200

    refactoring: passing parameters as possible many parameters '...numbers'; add mocha parameters in config file 'mocha.opts'

--- tutorial#12/lib/calculator.js
+++ tutorial#12/lib/calculator.js
@@ -2,8 +2,12 @@
 
 const calculator = {};
 
-calculator.add = function (left, right) {
-  return left + right;
+calculator.add = function (...numbers) {
+  if (!numbers || numbers.length === 0) {
+    throw new Error('Numbers are missing.');
+  }
+
+  return numbers.reduce((sum, number) => sum + number);
 };
 
 module.exports = calculator;
--- tutorial#12/test/calculatorTests.js
+++ tutorial#12/test/calculatorTests.js
@@ -9,3 +9,15 @@ test('add returns the sum of the given numbers.', () => {
 
   assert.that(sum).is.equalTo(65);
 });
+
+test('add returns the sum of the given numbers even when the first number is zero.', () => {
+  const sum = calculator.add(0, 23);
+
+  assert.that(sum).is.equalTo(23);
+});
+
+test('add throws an error if numbers are missing.', () => {
+  assert.that(() => {
+    calculator.add();
+  }).is.throwing('Numbers are missing.');
+});
